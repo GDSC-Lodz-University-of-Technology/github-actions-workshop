1ccdf4ad90f9e881e69695815a6f3380beca5d78
wslabosz <krodenakakrodex@gmail.com>
2022-11-24 19:46:53 +0100

    check

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ export const App = () => {
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-      <p>HMR works!</p>
+      <p>HMR works, and perhaps deploy!</p>
     </div>
   );
 };
