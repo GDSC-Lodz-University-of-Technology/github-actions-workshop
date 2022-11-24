feadf04284ed2c9b780ef6bcbfd8e66380ebc6f6
wslabosz <krodenakakrodex@gmail.com>
2022-11-24 18:03:43 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ export const App = () => {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test.
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
