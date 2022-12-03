90d5038ea6ef1402c31813648facb1dcad94a3a5
wslabosz <krodenakakrodex@gmail.com>
2022-12-03 23:21:48 +0100

    abngertr

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/github-actions-workshop/',
   server: {
     port: 8080,
   },
