603e9b9c9e1c19fce84e7c959c700d1c8e76cb09
wslabosz <krodenakakrodex@gmail.com>
2022-12-03 23:12:12 +0100

    is that it

--- src/App.tsx
+++ src/App.tsx
@@ -26,9 +26,6 @@ export const App = () => {
           }}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR.
-        </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
       <p>this is my final solution</p>
