b33998c2a1b399c124ca0ad2c11edbcfb9c5c2c2
wslabosz <krodenakakrodex@gmail.com>
2022-12-03 22:38:23 +0100

    damn

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-import reactLogo from './assets/react.svg';
+import reactLogo from './react.svg';
 
 export const App = () => {
   const [count, setCount] = useState(0);
